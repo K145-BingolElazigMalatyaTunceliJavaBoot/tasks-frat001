3503f905812f2e44bd20b4b6d74ee9e35d00a6df
Fırat Özgür Pıçak <firatozgurp@gmail.com>
2022-09-24 00:00:55 +0300

    Düzenleme

--- HaftaBir/src/RastgeleSayi.java
+++ HaftaBir/src/RastgeleSayi.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 public class RastgeleSayi
 {
-    public static void main (String args [] ) {
-        for (int i=0; i<20; i++){
-            System.out.print (1+(int)(Math.random()* 499)+"\t");
+    public static void main (String args [] )
+    {
+        Random random = new Random();
+        int sayi = random.nextInt( 500);
+        if (sayi>50 && sayi<100)
+        {
+             System.out.println(sayi);
         }
-    }
+        else {
+            System.out.println(sayi);
+        }
 
+    }
 }
