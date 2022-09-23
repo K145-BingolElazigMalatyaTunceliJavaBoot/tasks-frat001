dccde04ebd3e45a93b23294db31fb06113bdaea8
Fırat Özgür Pıçak <firatozgurp@gmail.com>
2022-09-23 20:26:55 +0300

    3 İle bölümün dışındaki sayılar

---
+++ HaftaBir/src/RastgeleSayi.java
@@ -0,0 +1,9 @@
+public class RastgeleSayi
+{
+    public static void main (String args [] ) {
+        for (int i=0; i<20; i++){
+            System.out.print (1+(int)(Math.random()* 499)+"\t");
+        }
+    }
+
+}
